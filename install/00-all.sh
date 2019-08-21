4f611d0ba827fd84745cfd7e7c2547676253e946
Your Name <you@example.com>
2019-08-21 16:30:07 +0100

    .

--- install/00-all.sh
+++ install/00-all.sh
@@ -1,4 +1,5 @@
 ./01-apts.sh
 ./02-audio.sh
 ./03-screen.sh
+./04-tweak.sh
 
---
+++ install/04-tweak.sh
@@ -0,0 +1,2 @@
+xfconf-query -c xfwm4 -p /general/titleless_maximize -s true
+xfconf-query -c xfwm4 -p /general/use_compositing -s false
