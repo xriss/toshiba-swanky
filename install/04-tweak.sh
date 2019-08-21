7872ef42c2ea183ba80d6e7ddad2ace4e9abc3f8
Your Name <you@example.com>
2019-08-21 16:35:44 +0100

    .

--- install/04-tweak.sh
+++ install/04-tweak.sh
@@ -1,2 +1,5 @@
 xfconf-query -c xfwm4 -p /general/titleless_maximize -s true
 xfconf-query -c xfwm4 -p /general/use_compositing -s false
+
+#kill the error reporting popup on boot
+sudo apt -y purge apport
