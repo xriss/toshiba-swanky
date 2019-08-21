5692caa92ce2179cd299b7285e25acfb8ded1248
Your Name <you@example.com>
2019-08-21 12:48:22 +0100

    .

---
+++ install/02-audio.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/plbossart/UCM.git
+sudo cp -r UCM/chtmax98090/ /usr/share/alsa/ucm/
+#sudo alsaucm -c chtmax98090 set _verb HiFi set _enadev Speakers
+#sudo alsactl store
+
+#sudo amixer -D 1 set Speaker 100%
+
