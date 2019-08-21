40369089fdb75616c617fee04e46e5a2ccc5afbc
Your Name <you@example.com>
2019-08-21 16:47:43 +0100

    install default apps

--- install/05-apps.sh
+++ install/05-apps.sh
@@ -1,3 +1,22 @@
 
+
+#dev
 sudo apt -y install geany geany-plugins
+sudo apt -y install node npm
+
+
+#art
+sudo apt -y install gimp
+sudo apt -y install inkscape
+sudo apt -y install blender
+
+
+#sound
+sudo apt -y install audacity
+sudo apt -y install lmms
+
+
+#wetgenes
+sudo snap install gamecake
+
 
