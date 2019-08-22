3b30372c577cd6a1efdef6a0c63a95facfd858f0
Your Name <you@example.com>
2019-08-22 04:04:52 +0100

    .

--- install/03-screen.sh
+++ install/03-screen.sh
@@ -5,3 +5,11 @@
 sudo apt -y purge light-locker
 sudo apt -y autoremove
 
+
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-enabled -s false
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/blank-on-battery -s 0
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/blank-on-ac -s 0
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/brightness-level-on-battery -s 100
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/brightness-level-on-ac -s 100
+
+
