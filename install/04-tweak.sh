eeba60fd82b209bbfad9e0f66976c99ac39aec00
Your Name <you@example.com>
2019-08-23 16:04:43 +0100

    .

--- install/04-tweak.sh
+++ install/04-tweak.sh
@@ -16,6 +16,7 @@ sudo apt -y autoremove
 # auto mount sdcard in home on boot with sd card plugged in
 # but do not fail if no sdcard exists
 sudo mkdir -p /sdcard
+sudo chown 1000:1000 /sdcard
 ln -s /sdcard ~/
 
 sudo sed -i '/\/dev\/mmcblk1p1/d' /etc/fstab
