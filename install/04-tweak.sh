01cad671e856e509b4bd155d1895b24dc580d520
Your Name <you@example.com>
2019-08-25 14:46:53 +0100

    enable guest login

--- install/04-tweak.sh
+++ install/04-tweak.sh
@@ -19,9 +19,9 @@ sudo apt -y autoremove
 # auto mount sdcard in home on boot with sd card plugged in
 # but do not fail if no sdcard exists
 
-sudo sed -i '/\/dev\/mmcblk1p1/d' /etc/fstab
+sudo sed -i '/^\/dev\/mmcblk1p1/d' /etc/fstab
 cat <<EOT | sudo tee -a /etc/fstab
-/dev/mmcblk1p1 /sdcard    vfat    umask=000,dmask=027,fmask=027,uid=1000,gid=1000,noatime,user,auto,nofail,rw,exec
+/dev/mmcblk1p1 /sdcard    vfat    umask=000,dmask=000,fmask=000,uid=1000,gid=1000,noatime,user,auto,nofail,rw,exec
 EOT
 
 sudo mkdir -p /sdcard
@@ -33,3 +33,9 @@ ln -s /sdcard ~/
 sudo cp weirdspace.jpg /usr/share/xfce4/backdrops/weirdspace.jpg
 xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitoreDP-1/workspace0/last-image --set /usr/share/xfce4/backdrops/weirdspace.jpg
 
+# enable guest login 
+
+sudo sed -i '/^allow-guest=/d' /usr/share/lightdm/lightdm.conf.d/50-disable-guest.conf
+cat <<EOT | sudo tee -a /usr/share/lightdm/lightdm.conf.d/50-disable-guest.conf
+allow-guest=true
+EOT
