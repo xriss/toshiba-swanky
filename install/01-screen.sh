544a5698ce293015e9528ea3b238cfbcbf19e5b9
Your Name <you@example.com>
2019-11-26 23:20:54 +0000

    .

--- install/01-screen.sh
+++ install/01-screen.sh
@@ -1,4 +1,16 @@
 
+# set boot options 
+# acpi=off fails to boot...
+
+sudo sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/d' /etc/default/grub
+cat <<EOT | sudo tee -a /etc/default/grub
+GRUB_CMDLINE_LINUX_DEFAULT=""
+EOT
+
+sudo update-grub
+
+
+
 # try and convince the screen not to blank, now and forever
 cat <<EOT | tee ~/.xinitrc
 sleep 10
@@ -46,14 +58,4 @@ xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/lid-action-on-ac --c
 # enable suspend
 sudo systemctl unmask sleep.target suspend.target hibernate.target hybrid-sleep.target
 
-
-# set boot options 
-# acpi=off fails to boot...
-
-sudo sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/d' /etc/default/grub
-cat <<EOT | sudo tee -a /etc/default/grub
-GRUB_CMDLINE_LINUX_DEFAULT=""
-EOT
-
-sudo update-grub
 
