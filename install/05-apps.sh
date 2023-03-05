54ba5ecc96ca2dc9e5f824e2385b05b9bee50995
kriss <kriss@xixs.com>
2023-03-05 01:25:24 +0000

    add firefox

--- install/05-apps.sh
+++ install/05-apps.sh
@@ -3,17 +3,23 @@
 # using snap is prefered as it gets the latest versions
 # while apt often lags far far behind
 
+
+#dev tools
+sudo DEBIAN_FRONTEND=noninteractive apt -y install firefox
+
 #dev tools
 sudo DEBIAN_FRONTEND=noninteractive apt -y install geany geany-plugins
 sudo DEBIAN_FRONTEND=noninteractive apt -y install nodejs npm
 sudo DEBIAN_FRONTEND=noninteractive apt -y install gparted
 sudo DEBIAN_FRONTEND=noninteractive apt -y install wavemon
+
 #sudo DEBIAN_FRONTEND=noninteractive apt -y install wine
 
 
 #art tools
 sudo DEBIAN_FRONTEND=noninteractive apt -y install gimp
 sudo DEBIAN_FRONTEND=noninteractive apt -y install inkscape
+
 #sudo DEBIAN_FRONTEND=noninteractive apt -y install blender
 
 
@@ -24,6 +30,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt -y install inkscape
 #sound tools
 sudo DEBIAN_FRONTEND=noninteractive apt -y install audacity
 sudo DEBIAN_FRONTEND=noninteractive apt -y install lmms
+
 #sudo DEBIAN_FRONTEND=noninteractive apt -y install fluidsynth
 #sudo DEBIAN_FRONTEND=noninteractive apt -y install vmpk
 #sudo DEBIAN_FRONTEND=noninteractive apt -y install amsynth
