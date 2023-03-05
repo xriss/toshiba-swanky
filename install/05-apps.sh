316f954207b07e664d0a5d59ff7687fb4e270526
kriss <kriss@xixs.com>
2023-03-05 10:56:24 +0000

    apps

--- install/05-apps.sh
+++ install/05-apps.sh
@@ -1,36 +1,42 @@
 
 # install heavy applications that we often use
-# using snap is prefered as it gets the latest versions
-# while apt often lags far far behind
+# using snap takes up more space which is a problem on this tiny laptop
 
 
-#dev tools
+
+# minimal install does not include firefox
 sudo DEBIAN_FRONTEND=noninteractive apt -y install firefox
 
-#dev tools
+
+
+# dev tools
 sudo DEBIAN_FRONTEND=noninteractive apt -y install geany geany-plugins
 sudo DEBIAN_FRONTEND=noninteractive apt -y install nodejs npm
 sudo DEBIAN_FRONTEND=noninteractive apt -y install gparted
 sudo DEBIAN_FRONTEND=noninteractive apt -y install wavemon
 
-#sudo DEBIAN_FRONTEND=noninteractive apt -y install wine
 
 
-#art tools
+# art tools
 sudo DEBIAN_FRONTEND=noninteractive apt -y install gimp
 sudo DEBIAN_FRONTEND=noninteractive apt -y install inkscape
 
-#sudo DEBIAN_FRONTEND=noninteractive apt -y install blender
 
 
-#wetgenes ( my code )
-#sudo snap install gamecake
+# wetgenes ( my code )
+sudo snap install gamecake
 
 
-#sound tools
+
+# sound tools
 sudo DEBIAN_FRONTEND=noninteractive apt -y install audacity
 sudo DEBIAN_FRONTEND=noninteractive apt -y install lmms
 
+
+
+# not installed
+#sudo DEBIAN_FRONTEND=noninteractive apt -y install wine
+#sudo DEBIAN_FRONTEND=noninteractive apt -y install blender
 #sudo DEBIAN_FRONTEND=noninteractive apt -y install fluidsynth
 #sudo DEBIAN_FRONTEND=noninteractive apt -y install vmpk
 #sudo DEBIAN_FRONTEND=noninteractive apt -y install amsynth
